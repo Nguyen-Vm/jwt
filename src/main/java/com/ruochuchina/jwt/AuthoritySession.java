901a2b2911644fd3fbf7241fa681a00499d6c8b5
nguyen <rwmingad@163.com>
2018-05-10 22:53:53 +0800

    jwt

---
+++ src/main/java/com/ruochuchina/jwt/AuthoritySession.java
@@ -0,0 +1,17 @@
+package com.ruochuchina.jwt;
+
+/**
+ * @author RWM
+ * @date 2018/5/10
+ */
+public class AuthoritySession {
+
+    public Integer pubkeyCode;
+
+    public Integer signatureCode;
+
+    public String userId;
+
+    public String roleId;
+
+}
--- src/main/java/com/ruochuchina/jwt/JwtAuthentication.java
+++ src/main/java/com/ruochuchina/jwt/JwtAuthentication.java
@@ -2,11 +2,16 @@ package com.ruochuchina.jwt;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTCreator;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.impl.PublicClaims;
 import com.auth0.jwt.interfaces.Verification;
 import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
 import org.nguyen.foun.utils.DateUtils;
+
+import java.util.Date;
+import java.util.List;
 
 public class JwtAuthentication {
 
@@ -32,7 +37,7 @@ public class JwtAuthentication {
                     .acceptLeeway(1L);
 
             this.algorithm = DesAlgorithm.create(secret);
-        }catch (Exception e) {
+        } catch (Exception e) {
             throw new RuntimeException("initial jwt authentication error......", e);
         }
     }
@@ -44,11 +49,62 @@ public class JwtAuthentication {
     public String calPubKey(String userId, String roleId) {
         long now = DateUtils.time();
         String msg = now % 2 == 0 ? joiner(userId, now, roleId) : joiner(roleId, now, userId);
-        return algorithm.decrypt(msg);
+        return algorithm.encrypt(msg);
     }
 
-    public static void main(String[] args) {
-        JwtAuthentication jwtAuthentication = new JwtAuthentication("ruochuchina");
+    private AuthoritySession verifyPubKey(String pubKey) {
+        String msg = algorithm.decrypt(pubKey);
+        List<String> msgList = Splitter.on(":").splitToList(msg);
+        if (msgList.size() == 3) {
+            boolean even = 0 == (Long.parseLong(msgList.get(1)) % 2);
+            AuthoritySession session = new AuthoritySession();
+            session.pubkeyCode = 200;
+            session.userId = even ? msgList.get(0) : msgList.get(2);
+            session.roleId = even ? msgList.get(2) : msgList.get(0);
+            return session;
+        }
+        throw new RuntimeException("verify pubKey error");
+    }
 
+    public String calSignature(String userId, String roleId, long expireTime) {
+        try {
+            Date iat = DateUtils.now();
+            Date exp = DateUtils.ofDate(iat.getTime() + expireTime);
+            return jwtBuilder.withClaim(PublicClaims.SUBJECT, joiner(userId, roleId))
+                    .withClaim(PublicClaims.ISSUED_AT, iat)
+                    .withClaim(PublicClaims.EXPIRES_AT, exp)
+                    .withClaim(PublicClaims.NOT_BEFORE, iat)
+                    .sign(Algorithm.HMAC256(secret));
+        } catch (Exception e) {
+            throw new RuntimeException("calculate signature error.....", e);
+        }
+    }
+
+    public AuthoritySession verifySignature(String pubKey, String signature) {
+        AuthoritySession session = verifyPubKey(pubKey);
+        if (200 == session.pubkeyCode) {
+            try {
+                JWTVerifier verifier = verification
+                        .withClaim(PublicClaims.SUBJECT, joiner(session.userId, session.roleId))
+                        .build();
+                verifier.verify(signature);
+                session.signatureCode = 200;
+            } catch (Exception e) {
+                throw new RuntimeException("verify signature error", e);
+            }
+        }
+        return session;
+    }
+
+    public static void main(String[] args) throws Exception {
+        JwtAuthentication jwtAuthentication = new JwtAuthentication("ruochuchina");
+        String pubKey = jwtAuthentication.calPubKey("user_id", "role_id");
+        System.out.println(pubKey);
+        AuthoritySession session = jwtAuthentication.verifyPubKey(pubKey);
+        System.out.println("userId:" + session.userId);
+        System.out.println("roleId:" + session.roleId);
+        String signature = jwtAuthentication.calSignature(session.userId, session.roleId, 1000);
+        Thread.sleep(2000);
+        System.out.println(jwtAuthentication.verifySignature(pubKey, signature).signatureCode);
     }
 }
